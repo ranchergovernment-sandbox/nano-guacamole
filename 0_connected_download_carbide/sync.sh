e4a846b0d37e25a605e4abb775eb13711749d55a
Will Stutt <will@bastion.linuxlabs.local>
2026-08-20 17:35:34 +0000

    Adding stig profile

--- 0_connected_download_carbide/sync.sh
+++ 0_connected_download_carbide/sync.sh
@@ -13,4 +13,6 @@ sync_product harvester ${HARVESTER_VERSION}
 
 sync_charts rancher rancher ${RANCHER_CHART_VERSION} oci://registry.ranchercarbide.dev/carbide-charts
 
+sync_charts rancher rgs-stig-profiles ${RANCHER_CHART_VERSION} oci://registry.ranchercarbide.dev/carbide-charts
+
 sync_charts airgapped-docs airgapped-docs ${AIRGAP_DOCS_CHART_VERSION} oci://registry.ranchercarbide.dev/carbide-charts
